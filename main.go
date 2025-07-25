dbc0e2ff7ebb30fd29ad898678f333ab8c4fba8c
Pattabiraman VK <pattabimanu@gmail.com>
2025-07-26 03:10:31 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,69 +1,78 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "html/template"
-    "log"
-    "net/http"
-    "os"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 type Profile struct {
-    Name     string `json:"name"`
-    Title    string `json:"title"`
-    Location string `json:"location"`
-    Summary  string `json:"summary"`
-    LinkedIn string `json:"linkedin"`
+	Name     string `json:"name"`
+	Title    string `json:"title"`
+	Location string `json:"location"`
+	Summary  string `json:"summary"`
+	LinkedIn string `json:"linkedin"`
 }
 
 var templates *template.Template
 var profile Profile
 
 func main() {
-    // Parse templates with layout support
-    templates = template.Must(template.ParseGlob("templates/*.html"))
+	var err error
+	// Parse all templates in the templates directory
+	templates, err = template.ParseGlob("templates/*.html")
+	if err != nil {
+		log.Fatalf("Error parsing templates: %v", err)
+	}
 
-    // Load profile data from JSON file
-    file, err := os.Open("data/profile.json")
-    if err != nil {
-        log.Fatalf("Error opening profile.json: %v", err)
-    }
-    defer file.Close()
-    if err := json.NewDecoder(file).Decode(&profile); err != nil {
-        log.Fatalf("Error decoding profile json: %v", err)
-    }
+	// Load profile data from JSON file
+	file, err := os.Open("data/profile.json")
+	if err != nil {
+		log.Fatalf("Error opening profile.json: %v", err)
+	}
+	defer file.Close()
+	if err := json.NewDecoder(file).Decode(&profile); err != nil {
+		log.Fatalf("Error decoding profile json: %v", err)
+	}
 
-    r := mux.NewRouter()
-    r.HandleFunc("/", homeHandler)
-    r.HandleFunc("/about", aboutHandler)
-    r.HandleFunc("/contact", contactHandler)
-    r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/about", aboutHandler)
+	r.HandleFunc("/contact", contactHandler)
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-    fmt.Println("Server running at http://localhost:8080")
-    log.Fatal(http.ListenAndServe(":8080", r))
+	fmt.Println("Server running at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, "home.html", profile)
+	renderTemplate(w, "home", profile)
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, "about.html", profile)
+	renderTemplate(w, "about", profile)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, "contact.html", profile)
+	renderTemplate(w, "contact", profile)
 }
 
-templates = template.Must(template.ParseGlob("templates/*.html"))
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-    err := templates.ExecuteTemplate(w, "layout", profile)
-    if err != nil {
-        log.Printf("Template error: %v", err)
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	// Always execute the layout, passing the block template name as the first argument
+	err := templates.ExecuteTemplate(w, "layout", struct {
+		Page    string
+		Profile Profile
+	}{
+		Page:    tmpl,
+		Profile: data.(Profile),
+	})
+	if err != nil {
+		log.Printf("Template render error: %v\n", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
